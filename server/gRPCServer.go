c2b9424c4595834a50f9821ca48d3520d2f54f64
Abolfazl Eslami <ab.eslami2001@gmail.com>
2025-07-12 10:54:50 +0330

    Feat: add keep alive for gRPC server

--- server/gRPCServer.go
+++ server/gRPCServer.go
@@ -1,14 +1,15 @@
 package server
 
 import (
-	"fmt"
 	"github.com/eslami200117/ala_unlimited/config"
 	"github.com/eslami200117/ala_unlimited/pkg/comm"
 	"github.com/eslami200117/ala_unlimited/protocpb"
 	"github.com/eslami200117/ala_unlimited/service"
 	"github.com/rs/zerolog"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 	"net"
+	"time"
 )
 
 type GRPCServer struct {
@@ -23,15 +24,27 @@ func NewGRPCServer() *GRPCServer {
 }
 
 func (g *GRPCServer) StartGRPC(core *service.Core, conf *config.Config) {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", conf.GrpcPort))
+	lis, err := net.Listen("tcp", ":"+conf.GrpcPort)
 	if err != nil {
 		g.logger.Fatal().Err(err).Msg("failed to listen")
 	}
 
-	grpcServer := grpc.NewServer()
-	protocpb.RegisterPriceServiceServer(grpcServer, core)
+	kaEnforcement := keepalive.EnforcementPolicy{
+		MinTime:             60 * time.Second, // at least 60s between client pings
+		PermitWithoutStream: true,             // allow ping even if no active RPCs
+	}
+	kaParams := keepalive.ServerParameters{
+		Time:    60 * time.Second, // ping clients every 60s if idle
+		Timeout: 20 * time.Second, // wait 20s for ack before closing
+	}
 
-	g.logger.Info().Msg(fmt.Sprintf("gRPC server listening on :%s", conf.GrpcPort))
+	grpcServer := grpc.NewServer(
+		grpc.KeepaliveEnforcementPolicy(kaEnforcement),
+		grpc.KeepaliveParams(kaParams),
+	)
+
+	protocpb.RegisterPriceServiceServer(grpcServer, core)
+	g.logger.Info().Msg("gRPC server listening on :" + conf.GrpcPort)
 	if err := grpcServer.Serve(lis); err != nil {
 		g.logger.Fatal().Err(err).Msg("failed to serve")
 	}
